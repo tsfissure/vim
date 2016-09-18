9eaa9175aceb3f311945671e5a814e9bab1624ed
tsfissure <tsfissure@gmail.com>
2016-09-18 17:07:50 +0800

    add vim theme

--- copy.bat
+++ copy.bat
@@ -1,8 +1,8 @@
 echo off
-if not exist indent md indent
 copy D:\EditMachine\Vim\vim80\indent\cpp.vim .\indent\cpp.vim
 copy D:\EditMachine\Vim\vim80\indent\c.vim .\indent\c.vim
 copy D:\EditMachine\Vim\vim80\indent\python.vim .\indent\python.vim
+copy D:\EditMachine\Vim\vim80\colors\delek-tys.vim .\color\delek-tys.vim
 copy D:\EditMachine\Vim\_vimrc _vimrc
 
 pause
